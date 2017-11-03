fb16b805609bcc3c0928a3fd2e38d687fb49a4af
slastonm <slastonm@gmail.com>
2017-11-03 17:04:35 +0200

    TS settings

---
+++ HW1/ts-example.js
@@ -0,0 +1,6 @@
+var example = "string";
+var array = ["1", 1, "hello"];
+//let a = 5;
+for (var val in array) {
+    document.write(array[val]);
+}
---
+++ HW1/ts-example.ts
@@ -0,0 +1,6 @@
+var example = "string";
+let array = ["1", 1, "hello"];
+//let a = 5;
+for (var val in array){
+	document.write(array[val]);
+}
