5fe5140fe381e04e05e486afbb8ddcca35f93d14
slastonm <slastonm@gmail.com>
2017-11-05 00:19:05 +0200

    Add 5 HW but not work declare

---
+++ HW5/main.js
@@ -0,0 +1,6 @@
+/*** Created by Makc on 11/4/2017.*/
+function showValue() {
+    console.log(someGlobal);
+}
+showValue();
+//# sourceMappingURL=main.js.map
---
+++ HW5/main.ts
@@ -0,0 +1,7 @@
+/*** Created by Makc on 11/4/2017.*/
+
+ function showValue(){
+    console.log(someGlobal);
+}
+
+showValue()
